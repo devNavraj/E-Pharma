0b92c5d570c0882c47c08a868d9d547e217fcb20
Prabin <praabiin.rai@gmail.com>
2020-09-04 08:05:21 +0545

    heroku deploy

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -59,24 +59,24 @@ function HomeScreen(props) {
                     {
                     products.map(product =>
                         <li key={product._id}>
-                        <div className="product">
-                            <Link to={'/product/' + product._id}>
-                                <img className="products-image" src={product.image} alt="product" />
-                            </Link>
-                                
-                            <div className="product-name">
-                                    <Link to={'/product/' + product._id}>{product.name}</Link>
+                            <div className="product">
+                                <Link to={'/product/' + product._id}>
+                                    <img className="products-image" src={product.image} alt="product" />
+                                </Link>
                                     
+                                <div className="product-name">
+                                        <Link to={'/product/' + product._id}>{product.name}</Link>
+                                        
+                                </div>
+                                <div className="product-manufacturer">{product.manufacturer}</div>
+                                <div className="product-price">Rs.{product.price}</div>
+                                <div className="product-rating">
+                                    <Rating 
+                                        value={ product.rating } 
+                                        text={ product.numReviews + ' reviews' } 
+                                    />
+                                </div>
                             </div>
-                            <div className="product-manufacturer">{product.manufacturer}</div>
-                            <div className="product-price">Rs.{product.price}</div>
-                            <div className="product-rating">
-                                <Rating 
-                                    value={ product.rating } 
-                                    text={ product.numReviews + ' reviews' } 
-                                />
-                            </div>
-                        </div>
                         </li>)
                     }
                     

071c344620a471b556b96ae42d0d1725a21555c2
Prabin <praabiin.rai@gmail.com>
2020-09-07 18:38:04 +0545

    Server.js

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(config.PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log("Server started at http://localhost:5000");
 });
